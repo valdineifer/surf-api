eeb7ca454d752b052431b8c3e139ee2b26c3f287
Valdinei Ferreira <valdineifer@outlook.com>
2019-07-30 03:40:40 -0300

    add pagination to index method

--- controllers/SurfistaController.js
+++ controllers/SurfistaController.js
@@ -1,9 +1,21 @@
-const sequelize = require('../database');
-const Surfista = sequelize.import('../models/surfista');
+const sequelize = require("../database");
+const Surfista = sequelize.import("../models/surfista");
 
 module.exports = {
   async index(req, res) {
-    const surfistas = await Surfista.findAll();
+    const { page = 1 } = req.query;
+
+    if (page < 1) {
+      res.status(400).json({
+        erro: "Número de página inválido."
+      });
+    }
+
+    const pageSize = 10;
+    const offset = (page - 1) * pageSize; // Decremento para cálculo
+    const limit = offset + pageSize;
+
+    const surfistas = await Surfista.findAll({ offset: offset, limit: limit });
 
     res.json(surfistas);
   }
