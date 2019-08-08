ba07fe84b87891377d455c448800807667442845
Valdinei Ferreira <valdineifer@outlook.com>
2019-08-08 01:24:02 -0300

    create OndaController with index, store and destroy
    * freeze table name, prevent plural queries with Sequelize

--- models/Onda.js
+++ models/Onda.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     },
-    { sequelize, modelName: "Onda" }
+    { sequelize, modelName: "Onda", freezeTableName: true }
   );
   Onda.associate = function(models) {
     Onda.belongsTo(Bateria, { foreignKey: "bateriaId" });
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const SurfistaController = require('./controllers/SurfistaController');
 const BateriaController = require('./controllers/BateriaController');
+const OndaController = require('./controllers/OndaController');
 
 router.get("/", (req, res) => {
   res.json("Access the documentation to use the API correctly");
@@ -15,5 +16,9 @@ router.delete("/surfistas/:num", SurfistaController.destroy);
 
 router.get("/baterias", BateriaController.index);
 router.post("/baterias", BateriaController.store);
+
+router.get("/ondas", OndaController.index);
+router.post("/ondas", OndaController.store);
+router.delete("/ondas/:id", OndaController.destroy);
 
 module.exports = router;
